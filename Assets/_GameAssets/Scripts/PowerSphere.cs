b1379dac08d18a3daddfd17d439ebcf8761276f4
Fernando Paniagua <fernando.paniagua@gmail.com>
2020-09-29 14:07:13 +0200

    Sesion 2020/09/29

---
+++ Assets/_GameAssets/Scripts/PowerSphere.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerSphere : MonoBehaviour
+{
+    [SerializeField]
+    GameObject panel;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            panel.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            panel.SetActive(false);
+        }
+    }
+}
---
+++ Assets/_GameAssets/Scripts/Torch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torch : MonoBehaviour
+{
+    [SerializeField]
+    Light torch;
+
+    private void Awake()
+    {
+        torch.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            torch.enabled = !torch.enabled;
+        }    
+        if (Input.GetKeyDown(KeyCode.KeypadPlus))
+        {
+            torch.intensity = torch.intensity + 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.KeypadMinus))
+        {
+            torch.intensity = torch.intensity - 1;
+        }
+    }
+}
