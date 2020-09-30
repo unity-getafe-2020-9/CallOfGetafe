c8c52094424b847763326b802a70ab06710e4fae
Fernando Paniagua <fernando.paniagua@gmail.com>
2020-09-30 14:06:59 +0200

    Sesion 2020/09/30

---
+++ Assets/_GameAssets/Scripts/ColourChanger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnMouseDown()
+    {
+        //GetComponent<Renderer>().sharedMaterial.SetColor("_Color", Color.blue);
+        GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+    }
+}
