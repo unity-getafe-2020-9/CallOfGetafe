2470178275d73c1865103ba237244cb8b73731b8
Fernando Paniagua <fernando.paniagua@gmail.com>
2020-10-02 14:08:37 +0200

    2020/10/02
    Iluminación básica
    Introducción a las animaciones
    Cambio de Escena
    ProBuilder
    Disparos de proyectiles físicos

---
+++ Assets/_GameAssets/Scripts/Gun.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject proyectil;
+    public Transform posInicio;
+    public float fuerza;
+    
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject bala = Instantiate(proyectil, posInicio.position, posInicio.rotation);
+            bala.GetComponent<Rigidbody>().AddForce(posInicio.forward * fuerza);
+        }
+    }
+}
